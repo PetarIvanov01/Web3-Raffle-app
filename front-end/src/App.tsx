f94e67eedd9b2169b6a3e51c5643609c7794f125
PetarIvanov01 <123456789petar45@gmail.com>
2024-08-17 21:21:57 +0300

    Implement Raffle DApp structure with Web3 and Raffle context providers
    - Add main `App` component with conditional rendering based on MetaMask installation.
    - Include informative UI elements for welcoming users and guiding them to install MetaMask if not detected.
    - Wrap main `Layout` component with `Web3ContextProvider` and `RaffleContextProvider` for state management.
    - Enhance user experience with a responsive and visually appealing interface.

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,3 +1,48 @@
+import Layout from "./components/layout";
+import RaffleContextProvider from "./providers/RaffleContextProvider";
+import Web3ContextProvider from "./providers/Web3ContextProvider";
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div className="flex min-h-screen justify-center items-center">
+      <div className="container p-8 bg-gray-50 rounded-lg shadow-lg">
+        <section className="mb-8 text-center">
+          <h1 className="text-3xl font-bold text-gray-800">
+            Welcome to the Raffle DApp
+          </h1>
+          <p className="text-gray-600 mt-2">
+            Join the raffle to win exciting prizes! Remember, the more you
+            participate, the higher your chances of winning!
+          </p>
+        </section>
+        {window.ethereum ? (
+          <Web3ContextProvider>
+            <RaffleContextProvider>
+              <Layout />
+            </RaffleContextProvider>
+          </Web3ContextProvider>
+        ) : (
+          <>
+            <section className="mb-8 text-center bg-red-100 p-6 rounded-lg shadow-md">
+              <h2 className="text-2xl font-semibold text-red-600 mb-2">
+                MetaMask Not Installed
+              </h2>
+              <p className="text-gray-700">
+                It seems that you do not have MetaMask installed. Please install
+                MetaMask to participate in the raffle.
+              </p>
+              <a
+                href="https://metamask.io/download.html"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="inline-block mt-4 px-6 py-3 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600"
+              >
+                Install MetaMask
+              </a>
+            </section>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
